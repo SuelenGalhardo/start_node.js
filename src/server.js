fe43e4048f47a9444b9e80e920ea6bd6c74ba27e
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-17 12:03:23 +0200

    added  route params

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
-const express = require("express")
+const express = require("express");
 
-const app =  express();
+const app = express();
 
-app.get("/", (request, response) => {
-
-    response.send("hello word");
-
-})
+app.get("/message/:id/:user", (request, response) => {
+    
+    const { id, user } = request.params
+  response.send(`
+        Id da mensagem: ${id}.
+        Para o usuário: ${user}.
+                
+     `);
+});
 
 const PORT = 3333;
 
