7338d3049b1f12adb9879bfe44780f03d8a92532
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-17 12:44:02 +0200

    added  query params

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/message/:id/:user", (request, response) => {
-    
+
     const { id, user } = request.params
   response.send(`
         Id da mensagem: ${id}.
@@ -12,6 +12,12 @@ app.get("/message/:id/:user", (request, response) => {
      `);
 });
 
+app.get("/users", (request, response)=> {
+
+    const {page, limit} = request.query;
+    
+    response.send(`Pagina: ${page}. Mostrar: ${limit}`);
+})
 const PORT = 3333;
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
