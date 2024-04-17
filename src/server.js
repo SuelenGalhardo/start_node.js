750684b20a1d8e8744652ee67ea8f4159dd7c7fc
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-17 13:15:54 +0200

    added  insomnia and post

--- src/server.js
+++ src/server.js
@@ -2,21 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.get("/message/:id/:user", (request, response) => {
 
-    const { id, user } = request.params
-  response.send(`
-        Id da mensagem: ${id}.
-        Para o usuário: ${user}.
-                
-     `);
-});
+app.post("/users", (request, response)=> {
 
-app.get("/users", (request, response)=> {
-
-    const {page, limit} = request.query;
     
-    response.send(`Pagina: ${page}. Mostrar: ${limit}`);
+    response.send("você chamou o post");
 })
 const PORT = 3333;
 
