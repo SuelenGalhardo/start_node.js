a8efed45b72920fe2a8168bac79d0236f2af9025
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-17 13:50:53 +0200

    added  src = controllers

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,18 @@
+class UsersController {
+
+
+    /**
+   * index - GET para listar vários registros.
+   * show - GET para exibir um registro específico.
+   * create - POST para criar um registro.
+   * update - PUT para atualizar um registro.
+   * delete - DELETE para remover um registro.
+   */
+}
+
+
+module.exports = UsersController;
+
+
+//porque usamos class e nao uma funçao? porque a class permite que dentro dela 
+// eu posso ter varias funçoes e tbm acessar varias funçoes. constroller pode ter nomaximo 5 metodos. 
