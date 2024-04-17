6bbd77d729136c9c8a08990f49adc4abe6718efe
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-17 13:25:18 +0200

    added  insomnia post, json body params

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 
 app.post("/users", (request, response)=> {
-
+const { name, email, password} = request.body;
     
-    response.send("você chamou o post");
+    //response.send(`Usuario: ${name}. E-mail: ${email}. e password: ${password}`);
+    response.json({name, email, password});
 })
 const PORT = 3333;
 
