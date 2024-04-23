ffe55112316c869b37a21cae54666f9af6ea5637
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-23 23:37:25 +0200

    added update user data

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,45 +1,59 @@
-
-const { hash } = require("bcryptjs");
-const AppError = require("../utils/AppError");
-const sqliteConnection =require("../database/sqlite");
-
+const { hash } = require('bcryptjs')
+const AppError = require('../utils/AppError')
+const sqliteConnection = require('../database/sqlite')
 
 class UsersController {
+  async create(request, response) {
+    const { name, email, password } = request.body
 
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-    async  create(request, response){
-       const { name, email, password} = request.body;
-  
-
-       const database = await sqliteConnection();
-       const checkUsersExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
-
-       if(checkUsersExists){
-
-        throw new AppError("este email ja esta em uso. ")     
-    
+    if (checkUserExists){
+      throw new AppError("Este e-mail já está em uso.")
     }
 
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await hash(password, 8)
 
-    await database.run("INSERT INTO users(name, email, password ) VALUES(?, ?, ?)",
-    [name, email, hashedPassword]
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, hashedPassword]
+    )
 
-    );
+    return response.status(201).json()
+  }
 
+  async update(request, response) {
+     const { name, email } = request.body
+     const { id } = request.params
 
-    return response.status(201).json();
+     const database = await sqliteConnection()
+     const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
 
+     if(!user) {
+      throw new AppError("Usuário não encontrado")
+     }
 
+     const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+     if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.")
+     }
+
+     user.name = name
+     user.email= email
+
+     await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`, 
+      [user.name, user.email, new Date(), id]
+    )
+
+    return response.json()
+  }
 }
 
-    
-}
-
-
-module.exports = UsersController;
-
-
-//porque usamos class e nao uma funçao? porque a class permite que dentro dela 
-// eu posso ter varias funçoes e tbm acessar varias funçoes. constroller pode ter nomaximo 5 metodos. 
-
+module.exports = UsersController
