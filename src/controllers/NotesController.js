8252d1dba5b28d68c7b45b826bf6bbfbf9818efd
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-24 15:05:03 +0200

    added list notes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,54 +1,69 @@
-const knex = require("../database/knex");
+const knex = require("../database/knex")
 
 class NotesController {
   async create(request, response) {
-    const { title, description, tags, links } = request.body;
-    const { user_id } = request.params;
+    const { title, description, tags, links } = request.body
+    const { user_id } = request.params
 
     const [note_id] = await knex("notes").insert({
       title,
       description,
-      user_id,
-    });
+      user_id
+    })
 
-    const linksInsert = links.map((link) => {
+    const linksInsert = links.map(link => {
       return {
         note_id,
-        url: link,
-      };
-    });
+        url: link
+      }
+    })
 
-    await knex("links").insert(linksInsert);
+    await knex("links").insert(linksInsert)
 
-    const tagsInsert = tags.map((name) => {
+    const tagsInsert = tags.map(name => {
       return {
         note_id,
         name,
-        user_id,
-      };
-    });
+        user_id
+      }
+    })
 
-    await knex("tags").insert(tagsInsert);
+    await knex("tags").insert(tagsInsert)
 
-    response.json();
+    response.json()
   }
+
   async show(request, response) {
-    const { id } = request.params;
-    const note = await knex("notes").where({ id }).first();
-    const tags = (await knex("tags")).where({ note_id: id }).orderBy("name");
-    const links = await knex("links")
-      .where({ note_id: id })
-      .orderBy("created_at");
+    const { id } = request.params
 
-    return response.json({ ...note, tags, links });
+    const note = await knex("notes").where({ id }).first()
+    const tags = await knex("tags").where({ note_id: id }).orderBy("name")
+    const links = await knex("links").where({ note_id: id }).orderBy("created_at")
+
+    return response.json({
+      ...note,
+      tags,
+      links
+    })
   }
+
   async delete(request, response) {
-    const { id } = request.params;
+    const { id } = request.params
 
-    await knex("notes").where({ id }).delete();
+    await knex("notes").where({ id }).delete()
 
-    return response.json();
+    return response.json()
+  }
+
+  async index(request, response) {
+    const { user_id } = request.query
+
+    const notes = await knex("notes")
+    .where({ user_id })
+    .orderBy("title")
+
+    return response.json(notes)
   }
 }
 
-module.exports = NotesController;
+module.exports = NotesController
