fa515f6d5ead20e7bae19f8de910b332945b594b
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-24 14:35:30 +0200

    added delete notes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -32,11 +32,22 @@ class NotesController {
 
     response.json();
   }
-  async show(request, response){
+  async show(request, response) {
     const { id } = request.params;
-    const note = await knex("notes").where({id}).first();
-    return response.json(note);
-    
+    const note = await knex("notes").where({ id }).first();
+    const tags = (await knex("tags")).where({ note_id: id }).orderBy("name");
+    const links = await knex("links")
+      .where({ note_id: id })
+      .orderBy("created_at");
+
+    return response.json({ ...note, tags, links });
+  }
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex("notes").where({ id }).delete();
+
+    return response.json();
   }
 }
 
