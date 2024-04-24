ed0f381712421da992372c5c4629f11cbdae1405
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-24 10:41:38 +0200

    added config knex

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+
+const path = require("path");
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    useNullDefault: true
+  }
+
+  
+
+};
