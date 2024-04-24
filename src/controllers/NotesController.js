420bc0ef5c5ce376fd61a878f32237d6a1fc5713
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-24 13:45:25 +0200

    added register show note

---
+++ src/controllers/NotesController.js
@@ -0,0 +1,43 @@
+const knex = require("../database/knex");
+
+class NotesController {
+  async create(request, response) {
+    const { title, description, tags, links } = request.body;
+    const { user_id } = request.params;
+
+    const [note_id] = await knex("notes").insert({
+      title,
+      description,
+      user_id,
+    });
+
+    const linksInsert = links.map((link) => {
+      return {
+        note_id,
+        url: link,
+      };
+    });
+
+    await knex("links").insert(linksInsert);
+
+    const tagsInsert = tags.map((name) => {
+      return {
+        note_id,
+        name,
+        user_id,
+      };
+    });
+
+    await knex("tags").insert(tagsInsert);
+
+    response.json();
+  }
+  async show(request, response){
+    const { id } = request.params;
+    const note = await knex("notes").where({id}).first();
+    return response.json(note);
+    
+  }
+}
+
+module.exports = NotesController;
