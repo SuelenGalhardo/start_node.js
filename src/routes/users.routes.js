04206e9f3592e0c5d998ef56fa2f338e192afb6b
SuelenGalhardo <suelenranucci@gmail.com>
2024-05-14 12:02:28 +0200

    added test bug upload image

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,36 +1,25 @@
-const { Router, response, request } = require('express');
-const multer = require("multer")
-const uploadConf = require("../configs/upload")
+const { Router, response, request } = require("express");
+const multer = require("multer");
+const uploadConf = require("../configs/upload");
 
-
-const UsersController = require('../controllers/UsersController');
+const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const UserAvatarController = require("../controllers/UserAvatarController");
 
 const usersRoutes = Router();
 
-const upload = multer(uploadConf.MULTER)
-
+const upload = multer(uploadConf.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
-
-usersRoutes.post('/', usersController.create);
-usersRoutes.put('/',ensureAuthenticated, usersController.update); 
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response)=> {
-    //console.log(request.file.filename)
-    //response.json();
-    try {
-        if (!request.file) {
-            return response.status(400).json({ error: 'No file uploaded.' });
-        }
-        
-        console.log('File uploaded successfully:', request.file.filename);
-        response.json({ message: 'File uploaded successfully.' });
-    } catch (error) {
-        console.error('Error saving file:', error);
-        response.status(500).json({ error: 'Failed to save file.' });
-    }
-})
-
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  userAvatarController.update
+);
 
 module.exports = usersRoutes;
